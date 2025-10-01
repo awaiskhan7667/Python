d1e161aa1761b3a15a6eff751052eef26b5b7bc1
Awais khan <awaiscr60@gmail.com>
2025-10-01 12:46:22 +0500

    Add files via upload

---
+++ list_tuple.py
@@ -0,0 +1,32 @@
+# marks  = [12,3,4,5]
+# Note String is immutable in python but list is mutable in python in the below example i have changed the value of one item on the list.
+# marks[2] = 8
+# print(marks[:-1])
+
+
+# List Methods
+
+# student_marks = [67,87,89,77,88,98]
+
+# student_marks.append(65)
+# print(student_marks)
+
+# student_marks.sort()
+# print(student_marks)
+
+# student_marks.sort(reverse=True)
+# print(student_marks)
+# element = 898
+# student_marks.insert(0,element)
+# print(student_marks)
+
+
+tup = (2,2,4,5,6,3)
+# Immutable
+# tup[0] = 232
+# slicing
+# tup[::3]
+# print(tup[::-1])
+# Methods of tuple
+print(tup.index(4))
+print(tup.count(3))
